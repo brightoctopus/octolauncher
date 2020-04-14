5558dad169ec1dfaa93193dd2b9e41524c1fb649
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-04-14 12:05:03 +0100

    Add app/template.rb

---
+++ app/template.rb
@@ -0,0 +1,12 @@
+copy_file 'app/assets/stylesheets/application.scss'
+remove_file 'app/assets/stylesheets/application.css'
+
+copy_file 'app/assets/images/logo.png'
+
+copy_file 'app/controllers/home_controller.rb'
+copy_file 'app/helpers/layout_helper.rb'
+copy_file 'app/views/layouts/application.html.erb', force: true
+copy_file 'app/views/shared/_flash.html.erb'
+copy_file 'app/views/shared/_footer.html.erb'
+copy_file 'app/views/shared/_navbar.html.erb'
+copy_file 'app/views/home/index.html.erb'
