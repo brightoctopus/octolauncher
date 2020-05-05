ed84f8ac6862238cab2ce9b3850e43a37664c512
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-05-05 20:24:34 +0100

    Set rspec as the default test framrwork

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,12 @@
 insert_into_file 'config/application.rb', before: /^  end/ do
   <<-'RUBY'
+    config.generators do |g|
+      g.test_framework :rspec
+      g.system_tests    false
+      g.stylesheets     false
+      g.helper          false
+    end
+
     # Use sidekiq to process Active Jobs (e.g. ActionMailer's deliver_later)
     config.active_job.queue_adapter = :sidekiq
   RUBY
